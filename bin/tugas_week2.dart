ab0b0a9c357ef080562d30fd023b3d8cd8d2f34c
AriPurwoAji <ajiaripurwo@gmail.com>
2025-09-26 21:38:24 +0700

    Latihan

---
+++ bin/tugas_week2.dart
@@ -0,0 +1,5 @@
+import 'package:tugas_week2/tugas_week2.dart' as tugas_week2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tugas_week2.calculate()}!');
+}
---
+++ test/tugas_week2_test.dart
@@ -0,0 +1,8 @@
+import 'package:tugas_week2/tugas_week2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
