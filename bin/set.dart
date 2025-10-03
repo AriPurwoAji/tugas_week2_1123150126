bfc6a4cc2c9094d5f6467ac00d1c358e28a8b5e8
AriPurwoAji <ajiaripurwo@gmail.com>
2025-10-03 21:10:35 +0700

    update

---
+++ bin/set.dart
@@ -0,0 +1,10 @@
+void main() {
+  //set adl kumpulan element yang unit (tidak ada duplikat)
+
+  var angka = {1, 2, 3, 4, 5};
+  print(angka);
+
+  angka.add(6);
+  angka.add(2);
+  print(angka);
+}
