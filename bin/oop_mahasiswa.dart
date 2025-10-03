cbf7c7bfce6ceca5550ae9bdf9b1bc21898dc4d7
AriPurwoAji <ajiaripurwo@gmail.com>
2025-10-03 21:40:39 +0700

    latihan oop mahassiswa

---
+++ bin/oop_mahasiswa.dart
@@ -0,0 +1,14 @@
+class mahasiswa {
+  //atribut
+  String nama;
+  String nim;
+  String jurusan;
+  int angkatan;
+
+  mahasiswa({
+    required this.nama,
+    required this.nim,
+    required this.jurusan,
+    required this.angkatan,
+  });
+}
